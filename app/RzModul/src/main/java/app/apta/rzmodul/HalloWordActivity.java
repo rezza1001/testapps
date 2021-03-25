3395e0f0ce2bb004263fe4476ec60124d02a926a
Mochamad Rezza Gumilang <mochamad.gumilang@sibisnis.com>
2021-03-25 19:57:51 +0700

    first commit

---
+++ app/RzModul/src/main/java/app/apta/rzmodul/HalloWordActivity.java
@@ -0,0 +1,19 @@
+package app.apta.rzmodul;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Mochamad Rezza Gumilang on 25/Mar/2021.
+ * Class Info :
+ */
+
+public class HalloWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/app/apta/testapps/MainActivity.java
@@ -0,0 +1,17 @@
+package app.apta.testapps;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.irshulx.Editor;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
